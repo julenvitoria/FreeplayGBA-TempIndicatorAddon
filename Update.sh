37b7ba4689eb3fd6f34fa520078ce7b7895f7c7b
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-07-16 18:22:26 +0200

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 cd ~
-git clone https://github.com/julenvitoria/FreeplayGBAcm3
-cd FreeplayGBAcm3
-crontab mycron
+git clone https://github.com/julenvitoria/FreeplayGBAcm3Addons
+cd FreeplayGBAcm3Addons
 cp emulators.cfg /opt/retropie/configs/all
+cd ~
+git clone https://github.com/LuisDiazUgena/temperatureMonitor
+sudo apt update
+sudo apt install libpng-dev -y
+sudo apt-get install python-gpiozero -y
+sudo apt-get install python-pkg-resources python3-pkg-resources -y
+sudo apt-get install build-essential python-dev python-smbus python-pip -y
+sudo chmod 755 /home/pi/temperatureMonitor/Pngview/pngview
+cd ~/FreeplayGBAcm3Addons
+crontab mycron
